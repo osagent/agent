3808c26d5233237be4c1e36d21e6ddf18f624e4e
Vitor Leonel <falecom@vitorleonel.com>
2019-03-23 01:52:06 -0300

    fix return data

--- src/resources/health/index.js
+++ src/resources/health/index.js
@@ -1,17 +1,20 @@
 const os = require("os");
+const diskusage = require("diskusage");
 
 class Health {
   async index(req, res, next) {
     try {
+      const { total: diskTotal, free: diskFree } = await diskusage.check("/");
+
       const informations = {
-        uptime: os.uptime(),
-        cpu: {
-          cores: os.cpus().length,
-          description: os.cpus()[0].model
-        },
+        uptime: parseFloat((os.uptime() / 86400).toFixed(0)),
         memory: {
-          total: os.totalmem(),
-          free: os.freemem()
+          total: parseFloat((os.totalmem() / 1e9).toFixed(2)),
+          used: parseFloat(((os.totalmem() - os.freemem()) / 1e9).toFixed(2))
+        },
+        disk: {
+          total: parseFloat((diskTotal / 1e9).toFixed(2)),
+          used: parseFloat(((diskTotal - diskFree) / 1e9).toFixed(2))
         }
       };
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,13 @@ const router = require("express").Router();
 
 const { Health } = require("../resources");
 
-router.get("/", Health.index);
+router.get("/", (req, res) => {
+  res.json({
+    name: "OS Agent",
+    version: "0.0.1"
+  });
+});
+
+router.get("/health", Health.index);
 
 module.exports = router;
